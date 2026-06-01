9df932cd0548864a62b459df7e41fe0e6b5c89df
devEthan6737 <victorchacongarcia2006@gmail.com>
2026-06-01 13:30:07 +0200

    add: libs & main

---
+++ src/main/java/com/autosur/Main.java
@@ -0,0 +1,42 @@
+package main.java.com.autosur;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import main.java.com.autosur.database.DatabaseConnection;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            DatabaseConnection.initDatabase();
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/login.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            
+            if (getClass().getResource("/css/styles.css") != null) {
+                scene.getStylesheets().add(getClass().getResource("/css/styles.css").toExternalForm());
+            }
+
+            primaryStage.setTitle("AutoSur - Gestión de Taller");
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+
+        } catch (IOException e) {
+            System.err.println("Error al cargar la interfaz de login: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
